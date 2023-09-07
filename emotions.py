0becc9ff7b0089aaa41a16c852ac3a4a847aee4c
KeertiB07 <144361382+KeertiB07@users.noreply.github.com>
2023-09-07 21:01:05 +0530

    Update emotions.py

--- emotions.py
+++ emotions.py
@@ -1,1 +1,216 @@
+# FACE DETECTION IN IMAGE
+import cv2
+from google.colab import files
+from google.colab.patches import cv2_imshow
+
+# Upload the image file
+uploaded = files.upload()
+
+# Read the image
+image_path = next(iter(uploaded))
+image = cv2.imread(image_path)
+
+# Load the pre-trained face cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Convert the image to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Perform face detection
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+# Draw rectangles around the detected faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+# Display the image with face detections
+cv2_imshow(image)
+
+
+# LIVE FACE DETECTION
+import imutils
+import numpy as np
+import cv2
+from google.colab.patches import cv2_imshow
+from IPython.display import display, Javascript
+from google.colab.output import eval_js
+from base64 import b64decode
+def take_photo(filename='photo.jpg', quality=0.8):
+  js = Javascript('''
+    async function takePhoto(quality) {
+      const div = document.createElement('div');
+      const capture = document.createElement('button');
+      capture.textContent = 'Capture';
+      div.appendChild(capture);
+
+      const video = document.createElement('video');
+      video.style.display = 'block';
+      const stream = await navigator.mediaDevices.getUserMedia({video: true});
+
+      document.body.appendChild(div);
+      div.appendChild(video);
+      video.srcObject = stream;
+      await video.play();
+
+      // Resize the output to fit the video element.
+      google.colab.output.setIframeHeight(document.documentElement.scrollHeight, true);
+
+      // Wait for Capture to be clicked.
+      await new Promise((resolve) => capture.onclick = resolve);
+
+      const canvas = document.createElement('canvas');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      canvas.getContext('2d').drawImage(video, 0, 0);
+      stream.getVideoTracks()[0].stop();
+      div.remove();
+      return canvas.toDataURL('image/jpeg', quality);
+    }
+    ''')
+  display(js)
+  data = eval_js('takePhoto({})'.format(quality))
+  binary = b64decode(data.split(',')[1])
+  with open(filename, 'wb') as f:
+    f.write(binary)
+  return filename
+  image_file = take_photo()
+  #image = cv2.imread(image_file, cv2.IMREAD_UNCHANGED)
+image = cv2.imread(image_file)
+
+# resize it to have a maximum width of 400 pixels
+image = imutils.resize(image, width=400)
+(h, w) = image.shape[:2]
+print(w,h)
+cv2_imshow(image)
+!wget -N https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt
+!wget -N https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20170830/res10_300x300_ssd_iter_140000.caffemodel
+print("[INFO] loading model...")
+prototxt = 'deploy.prototxt'
+model = 'res10_300x300_ssd_iter_140000.caffemodel'
+net = cv2.dnn.readNetFromCaffe(prototxt, model)
+# resize it to have a maximum width of 400 pixels
+image = imutils.resize(image, width=400)
+blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+print("[INFO] computing object detections...")
+net.setInput(blob)
+detections = net.forward()
+for i in range(0, detections.shape[2]):
+
+	# extract the confidence (i.e., probability) associated with the prediction
+	confidence = detections[0, 0, i, 2]
+
+	# filter out weak detections by ensuring the `confidence` is
+	# greater than the minimum confidence threshold
+	if confidence > 0.5:
+		# compute the (x, y)-coordinates of the bounding box for the object
+		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+		(startX, startY, endX, endY) = box.astype("int")
+		# draw the bounding box of the face along with the associated probability
+		text = "{:.2f}%".format(confidence * 100)
+		y = startY - 10 if startY - 10 > 10 else startY + 10
+		cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+		cv2.putText(image, text, (startX, y),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+    cv2_imshow(image)
+
+# EMOTION RECOGNITION FROM IMAGE
+!pip install deepface
+from deepface import DeepFace
+import cv2
+import matplotlib.pyplot as plt
+img1= cv2.imread("/content/angry.jpeg")
+plt.imshow(img1[:,:,::-1])
+plt.show()
+result= DeepFace.analyze(img1,actions = ['emotion'])
+print(result)
+
+# EMOTION RECOGNITION FROM VIDEO
+pip install FER
+from fer import Video
+from fer import FER
+import os
+import sys
+import pandas as pd
+location_videofile = "/content/Video_One.mp4"
+face_detector = FER(mtcnn=True)
+input_video = Video(location_videofile)
+processing_data = input_video.analyze(face_detector, display=False)
+vid_df = input_video.to_pandas(processing_data)
+vid_df = input_video.get_first_face(vid_df)
+vid_df = input_video.get_emotions(vid_df)
+pltfig = vid_df.plot(figsize=(20, 8), fontsize=16).get_figure()
+angry = sum(vid_df.angry)
+disgust = sum(vid_df.disgust)
+fear = sum(vid_df.fear)
+happy = sum(vid_df.happy)
+sad = sum(vid_df.sad)
+surprise = sum(vid_df.surprise)
+neutral = sum(vid_df.neutral)
+
+emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+emotions_values = [angry, disgust, fear, happy, sad, surprise, neutral]
+
+score_comparisons = pd.DataFrame(emotions, columns = ['Human Emotions'])
+score_comparisons['Emotion Value from the Video'] = emotions_values
+score_comparisons
+
+
+# HUMAN EMOTION RECOGNITION LIVE
+
+pip install deepface
+from deepface import DeepFace
+import imutils
+import numpy as np
+import cv2
+from google.colab.patches import cv2_imshow
+from IPython.display import display, Javascript
+from google.colab.output import eval_js
+from base64 import b64decode
+def take_photo(filename='photo.jpg', quality=0.8):
+  js = Javascript('''
+    async function takePhoto(quality) {
+      const div = document.createElement('div');
+      const capture = document.createElement('button');
+      capture.textContent = 'Capture';
+      div.appendChild(capture);
+
+      const video = document.createElement('video');
+      video.style.display = 'block';
+      const stream = await navigator.mediaDevices.getUserMedia({video: true});
+
+      document.body.appendChild(div);
+      div.appendChild(video);
+      video.srcObject = stream;
+      await video.play();
+
+      // Resize the output to fit the video element.
+      google.colab.output.setIframeHeight(document.documentElement.scrollHeight, true);
+
+      // Wait for Capture to be clicked.
+      await new Promise((resolve) => capture.onclick = resolve);
+
+      const canvas = document.createElement('canvas');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      canvas.getContext('2d').drawImage(video, 0, 0);
+      stream.getVideoTracks()[0].stop();
+      div.remove();
+      return canvas.toDataURL('image/jpeg', quality);
+    }
+    ''')
+  display(js)
+  data = eval_js('takePhoto({})'.format(quality))
+  binary = b64decode(data.split(',')[1])
+  with open(filename, 'wb') as f:
+    f.write(binary)
+  return filename
+   image_file= take_photo()
+image = cv2.imread(image_file)
+image = imutils.resize(image, width=400)
+(h,w) = image.shape[:2]
+print(w,h)
+cv2_imshow(image)
+result = DeepFace.analyze(image, actions = ['emotion'])
+print(result)
+
 
